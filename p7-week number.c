d7bccb85c07c5a607a22efd0368dff70643990ec
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-02 11:06:00 +0530

    LAB2

--- p7-week number.c
+++ p7-week number.c
@@ -1,5 +1,37 @@
 #include<stdio.h>
 void main()
 {
-    
+    int n;
+    printf("Enter any number : ");
+    scanf("%d",&n);
+    if(n>=1 && n<=7)
+        {
+            printf("Entered number is a week number \n");
+            switch(n)
+            {
+                case 1:
+                    printf("Day = Monday \n");
+                    break;
+                case 2:
+                    printf("Day = Tuesday \n");
+                    break;
+                case 3:
+                    printf("Day = Wednesday \n");
+                    break;
+                case 4:
+                    printf("Day = Thursday \n");
+                    break;
+                case 5:
+                    printf("Day = Friday \n");
+                    break;
+                case 6:
+                    printf("Day = Satuarday \n");
+                    break;
+                case 7:
+                    printf("Day = Sunday \n");
+                    break;
+            }
+        }
+    else
+        printf("Entered number is NOT a week number \n");
 }
