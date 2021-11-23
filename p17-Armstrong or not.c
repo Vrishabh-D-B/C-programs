d1ab54f0a2741840391b3411c0b41b07ed2ae448
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-23 10:30:29 +0530

    lab

--- p17-Armstrong or not.c
+++ p17-Armstrong or not.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int n, rem, res1, res2;
+    int n, n_clone, rem, res1, res2;
     
     printf("Enter any number : ");
     scanf("%d",&n);
 
+    n_clone = n;
     while(n!=0)
     {
         rem = n % 10;
@@ -14,9 +15,9 @@ void main()
         n = n/10;
     }
 
-    if(res2==n)
-        printf("\nNumber is Armstrong.");
+    if(res2==n_clone)
+        printf("\nNumber is Armstrong.\n");
     else
-        printf("\nNot Armstrong");
+        printf("\nNot Armstrong\n");
 
 }
