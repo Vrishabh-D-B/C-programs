09189b5c0f466e1f1ae7a8c04e4fb29e74980c0a
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-26 17:13:46 +0530

    lab

--- p12-print table.c
+++ p12-print table.c
@@ -2,11 +2,11 @@
 void main()
 {
    int j,n;
-   printf("Input the number (Table to be calculated) : ");
+   printf("\tInput the number (Table to be calculated) : ");
    scanf("%d",&n);
    printf("\n");
    for(j=1;j<=10;j++)
    {
-     printf("%d X %d = %d \n",n,j,n*j);
+     printf("\t%d X %d = %d \n",n,j,n*j);
    }
 } 
