c9e5c957754e6fbf36f85d4055a0341f18edcc82
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-26 22:08:50 +0530

    lab

--- p18-fibonacci series.c
+++ p18-fibonacci series.c
@@ -8,11 +8,11 @@ void main()
 
     n1 = 0;
     n2 = 1;
-    printf("\t%d %d ",n1,n2);
+    printf("\t%d  %d ",n1,n2);
     for(i=2;i<n;i++)
     {
         n3 = n1 + n2;
-        printf("\t%d ",n3);
+        printf(" %d ",n3);
         n1 = n2;
         n2 = n3;
     }
