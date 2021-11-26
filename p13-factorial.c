7b8082a906021fa6c038500576ae154bc48bdcb5
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-26 20:54:13 +0530

    lab

--- p13-factorial.c
+++ p13-factorial.c
@@ -11,7 +11,7 @@ int main() {
         for (i = 1; i <= n; ++i) {
             fact *= i;
         }
-        printf("\tFactorial of %d = %llu \n", n, fact);
+    printf("\tFactorial of %d = %llu \n", n, fact);
     }
 
     return 0;
