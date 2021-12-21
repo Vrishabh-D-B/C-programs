b206256926b84c6e7d35884e4373080c85360995
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-12-21 12:18:50 +0530

    lab

--- p33-implement basic String ops.c
+++ p33-implement basic String ops.c
@@ -4,15 +4,18 @@
 int main(){
     char str1[10] = "Hello";
     char str2[10] = "World";
-    char rslt[20]; 
-
-    //printf("Concatinatimg strings str1 and str2 = %s",str1+str2);
+    char rslt[20] = "";
+    
+    printf("Concatinatimg strings str1 and str2 = %s\n",strcat(str1,str2));
     
     printf("Length of the string str1 = %d\n",strlen(str1));
-
-    printf("Copying str2 in rslt\n");
-    strcpy(rslt,str2);
-    printf("rslt = %s\n",rslt);
-
     
+    strcpy(rslt,str2);
+    printf("Copying str2 in rslt = %s\n",rslt);
+    
+
+    // strrev(str1);
+    // printf("Reversing a str1 = %s\n",str1);
+    
+    return 0;
 }
